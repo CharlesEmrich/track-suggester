905a57794bc8987f83a9e32ed472c6b5baee4c86
Charles Emrich <libertinistic@gmail.com>
2017-04-28 11:14:37 -0700

    add initial survey design and flesh out index.html somewhat

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//Business at the Top:
+// Variables to hold compatibility levels with given tracks:
+var RubyRailsPref = 0,
+    PHPDrupalPref = 0,
+    JavaAndroidPref = 0,
+    CSSDesignPref = 0,
+    CdotNetPref = 0;
+
+
+//Party in the Basement:
