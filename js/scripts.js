72c64b923b3546dde56111b7bee7a1400dc1fc89
Charles Emrich <libertinistic@gmail.com>
2017-04-28 12:26:12 -0700

    do maybe-final styling of the form in bootstrap, add js skeleton

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,134 @@ var RubyRailsPref = 0,
     CSSDesignPref = 0,
     CdotNetPref = 0;
 
+function processQ1(answer) {
+  switch (answer) {
+    case 1:
 
+      break;
+    case 2:
+
+      break;
+    case 3:
+
+      break;
+    case 4:
+
+      break;
+    default:
+
+  }
+}
+
+function processQ2(answer) {
+  switch (answer) {
+    case 1:
+
+      break;
+    case 2:
+
+      break;
+    case 3:
+
+      break;
+    case 4:
+
+      break;
+    default:
+
+  }
+}
+
+function processQ3(answer) {
+  switch (answer) {
+    case 1:
+
+      break;
+    case 2:
+
+      break;
+    case 3:
+
+      break;
+    case 4:
+
+      break;
+    default:
+
+  }
+}
+
+function processQ4(answer) {
+  switch (answer) {
+    case 1:
+
+      break;
+    case 2:
+
+      break;
+    case 3:
+
+      break;
+    case 4:
+
+      break;
+    default:
+
+  }
+}
+
+function processQ5(answer) {
+  switch (answer) {
+    case 1:
+
+      break;
+    case 2:
+
+      break;
+    case 3:
+
+      break;
+    case 4:
+
+      break;
+    default:
+
+  }
+}
+
+function processSubQ1 (answer) {
+  switch (answer) {
+    case 1:
+
+      break;
+    case 2:
+
+      break;
+    case 3:
+
+      break;
+    case 4:
+
+      break;
+    default:
+
+  }
+}
+
+function resolveAnswers () {
+
+}
 //Party in the Basement:
+$(function() {
+  $("form").submit(function(event) {
+
+    event.preventDefault();
+  });
+
+  //Handles hiding and showing of subQ1:
+  $("#question1").change(function() {
+    if (true) {
+      //Check if they said yes, if so show subQ1
+    }
+  })
+});
